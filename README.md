9f79f4c1621893fea21e1839005bc9cd736d2800
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-10-14 16:10:49 +0100

    Functions added

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # TooGo
 
-TooGo is a small collection of useful code snippets that saves a bit of typing and copy-paste. 
+TooGo is a small collection of useful code snippets that save a bit of typing and copy-paste. 
+
 ## Functions
-### 
+### Getenv
+Read enviroment variables and use a default value if it's missing. 
+`port := Getenv("PORT", ":3000")`
+
+### FaviconHandler
+Use in routing to serve the favicon. 
+`router.HandleFunc("/favicon.ico", faviconHandler)`
+
+### RoundToTwo
+Takes a float64 and returns a string, rounded to two decimal points.
+
+### TimeToDay
+Takes a unix time in int format and returns the day, abbrevated to three letters.
+
+### TimeToHour
+Takes a unix timestamp in int format and returns the time in 24 hour format.
--- toogo.go
+++ toogo.go
@@ -9,7 +9,7 @@ import (
 
 // Read Enviroment variable. If it's missing use default value.
 // Ex port := Getenv("PORT", ":3000")
-func getEnv(key, defaultValue string) string {
+func Getenv(key, defaultValue string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		return defaultValue
@@ -18,13 +18,18 @@ func getEnv(key, defaultValue string) string {
 }
 
 // Serve favicon. The file must be in the templates directory
-func faviconHandler(w http.ResponseWriter, r *http.Request) {
+func FaviconHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "templates/favicon.ico")
+}
+
+// Round float to two decimals and return as string
+func RoundToTwo(f float64) string {
+	return fmt.Sprintf("%0.2f", f)
 }
 
 // Formating
 // Extract abrevated day from timestamp
-func timeToDay(dt int) string {
+func TimeToDay(dt int) string {
 	layout := "Mon"
 	t := int64(dt)
 	f := time.Unix(t, 0).Format(layout)
@@ -32,14 +37,9 @@ func timeToDay(dt int) string {
 }
 
 // Extract time in 24 hour format from timestamp
-func timeToHour(dt int) string {
+func TimeToHour(dt int) string {
 	layout := "Mon 15:00"
 	t := int64(dt)
 	f := time.Unix(t, 0).Format(layout)
 	return f
-}
-
-// Round float to two decimals and return as string
-func roundToTwo(f float64) string {
-	return fmt.Sprintf("%0.2f", f)
 }
